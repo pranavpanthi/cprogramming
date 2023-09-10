29275f81273c795e738428062362c87a1ea7dbe8
Pranav Panthi <panthipranav@gmail.com>
2023-09-10 13:01:08 +0545

    Practice

---
+++ float.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    float a;
+    float b;
+    float division;
+    printf("enter the number: ");
+    scanf("%f",&a);
+    printf("enter the number :");
+    scanf("%f",&b);
+    division = a / b;
+    printf("the division is %f:",division);
+    return 0;
+}
---
+++ zukarberg.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    int b;
+    int sum;
+    printf("enter the number: ");
+    scanf("%d",&a);
+    printf("enter the number :");
+    scanf("%d",&b);
+    sum = a+b;
+    printf("the sum is %d:",sum);
+    return 0;
+}

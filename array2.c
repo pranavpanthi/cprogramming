b543921daf5fc295a3795731de5864895671ce86
Pranav Panthi <panthipranav@gmail.com>
2024-01-14 18:05:04 +0545

    Practice

--- array2.c
+++ array2.c
@@ -1,16 +1,33 @@
 #include <stdio.h>
 
-#define row 5
-#define column 2
+#define row 3
+#define column 3
+
 int main(void) {
-    int arr[row][column] = {{0,0},{1,2},{2,4},{3,6},{4,8}};
+    int arr[row][column];
     int i,j;
     for ( i = 0; i < row; i++){
         for ( j = 0; j < column; j++)
         {
-            printf("arr[%d[%d] = %d\n",i,j,arr[i][j]);
+            printf("arr[%d[%d] =",i+1,j+1);
+            scanf("%d", &arr[i][j]);
         }
     }
+    printf("\n");
+    for ( i = 0; i < row; i++)
+    {
+        for ( j = 0; j < column; j++)
+        {
+            printf("%d\t",arr[i][j]);
+            if (j==2)
+            {
+                printf("\n");
+            }
+            
+        }
+        
+    }
+    
     return 0;
     
 }

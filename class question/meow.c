dd515f72aac84e7821a0d516748644e8d64cc141
Pranav Panthi <panthipranav@gmail.com>
2024-01-19 20:41:53 +0545

    fix

--- class question/meow.c
+++ class question/meow.c
@@ -13,6 +13,6 @@ int main(void) {
     for (int i = 0; i < 3; i++)
     {
         printf("Meow \n");
-    }
+    }  
     
-}
+} 
---
+++ past questions/test1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    int a = 0;
+    a = 5<2 ? 4 : 3;
+    printf("%d", a);
+    return 0;
+}

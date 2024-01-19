c2d47b7d511a510b4fa44a19f28e50dc923daae4
Prahlad Panthi <prahladpanthi.work@gmail.com>
2024-01-20 02:08:23 +1100

    array exercise

---
+++ pranav_exercises/sum_of_array.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int sumOfArrays(int[]);
+
+int main(void) {
+    int numbers[5];
+    // to do: take 5 integers from user
+
+    printf("Total sum = %d\n", sumOfArrays(numbers));
+    return 0;
+}
+
+int sumOfArrays(int nums[]) {
+    // should return the sum of integers in the array nums
+    return 0;
+}

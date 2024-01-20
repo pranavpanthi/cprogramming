9c47b7a7fda5c665be79978f44af419ef83fae6e
Pranav Panthi <panthipranav@gmail.com>
2024-01-20 09:00:06 +0545

    Solution to the sum of array

--- pranav_exercises/arrays/sum_of_array.c
+++ pranav_exercises/arrays/sum_of_array.c
@@ -5,6 +5,11 @@ int sumOfArrays(int[]);
 int main(void) {
     int numbers[5];
     // to do: take 5 integers from user
+    for (int i = 0; i < 5; i++)
+    {
+        printf("Enter number %d: ", i+1);
+        scanf("%d", &numbers[i]);
+    }
 
     printf("Total sum = %d\n", sumOfArrays(numbers));
     return 0;
@@ -12,5 +17,10 @@ int main(void) {
 
 int sumOfArrays(int nums[]) {
     // should return the sum of integers in the array nums
-    return 0;
+    int sum = 0;
+    for (int i = 0; i < 5; i++)
+    {
+        sum =sum+nums[i];
+    }
+    return sum;
 }

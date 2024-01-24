b2aa91fa112f004dcde5e1a5d34d2ec03f0eaff4
Pranav Panthi <panthipranav@gmail.com>
2024-01-24 19:43:49 +0545

    Even odd exercise done

--- pranav_exercises/arrays/even_odd.c
+++ pranav_exercises/arrays/even_odd.c
@@ -2,11 +2,28 @@
 
 int main(void) {
     int nums[10];
-    printf("Enter 10 positive integers: ");
+    printf("Enter 10 positive integers: \n");
     for (int i = 0; i < 10; ++i) {
+        printf("The integer [%d] is : ", i + 1);
         scanf("%d", &nums[i]);
     }
-
+    printf("Even array = ");
+    for (int i = 0; i < 10; i++)
+    {
+        if (nums[i] % 2 == 0)
+        {
+            printf("%d \t", nums[i]);
+        }
+    }
+    printf("\n Odd array = ");
+    for (int i = 0; i < 10; i++)
+    {
+        if (nums[i] % 2 == 1)
+        {
+            printf("%d \t", nums[i]);
+        }
+    }
+    
     // write the logic to store the even numbers and odd numbers in two separate arrays and print them
     // for example:
     // if nums = 1,2,3,4,5,6,7,8,9,10

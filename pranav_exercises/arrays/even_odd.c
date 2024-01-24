28d6debb0014ac7dd0ea4ffad8f5263cd2089b29
Prahlad Panthi <prahladpanthi.work@gmail.com>
2024-01-24 22:13:51 +1100

    added a problem related to arrays

---
+++ pranav_exercises/arrays/even_odd.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(void) {
+    int nums[10];
+    printf("Enter 10 positive integers: ");
+    for (int i = 0; i < 10; ++i) {
+        scanf("%d", &nums[i]);
+    }
+
+    // write the logic to store the even numbers and odd numbers in two separate arrays and print them
+    // for example:
+    // if nums = 1,2,3,4,5,6,7,8,9,10
+    // then we have to print:
+    // even array = 2, 4, 6, 8, 10
+    // odd array = 1, 3, 5, 7, 9
+
+    // write your solution below:
+
+    return 0;
+}

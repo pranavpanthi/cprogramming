a32d6a5a151b13c1d4a31b6d861305f792e01f99
Prahlad Panthi <prahladpanthi.work@gmail.com>
2024-01-26 22:20:55 +1100

    added a problem related to arrays

---
+++ pranav_exercises/arrays/add_matrix.c
@@ -0,0 +1,23 @@
+/*
+    write a program to take a square matrix from a user of any size (less than 10x10)
+    1. store the two matrices into two 2D arrays
+    2. add the two matrices
+    3. print the result
+
+    example:
+    enter dimension of the matrix: 3 3
+    enter matrix A:
+    1 2 3
+    4 5 6
+    7 8 9
+
+    enter matrix B:
+    1 1 1
+    1 1 1
+    1 1 1
+
+    Sum is:
+    2 3 4
+    5 6 7
+    8 9 10
+*/

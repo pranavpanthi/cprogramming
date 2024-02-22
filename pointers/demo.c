01cc66991941c68df7d62f997ce84664f20fd864
Pranav Panthi <panthipranav@gmail.com>
2024-02-22 17:46:49 +0545

    Pointer demo

--- pointers/demo.c
+++ pointers/demo.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
 int main(void) {
-    
+    int a = 10;
+    int *pc, **p ;
+    pc = &a;
+    p = &pc;
+    printf("The value of a is : %d\n", a);
+    printf("The value is %d\n", *pc);
+    printf("The value is : %d\n",**p);
     return 0;
 }
